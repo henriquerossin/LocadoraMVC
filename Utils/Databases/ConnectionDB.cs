e70baf617a41d863bbed682ad6c4ed90f56647af
Henrique <pro.henriquerossin@gmail.com>
2025-11-19 11:29:06 -0300

    fix connection string in ConnectionDB

--- Utils/Databases/ConnectionDB.cs
+++ Utils/Databases/ConnectionDB.cs
@@ -3,7 +3,7 @@
     public class ConnectionDB
     {
         private static readonly string _connectionString =
-            "Data Source=localhost;Persist Security Info=True;User ID=sa;Password=SqlServer@2022;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True";
+            "Data Source=localhost;Initial Catalog=LocadoraBD;Persist Security Info=True;User ID=sa;Password=SqlServer@2022;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True";
 
         public static string GetConnectionString()
         {
