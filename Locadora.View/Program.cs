2672f04873d0646038e4e8603a162184f141788f
Henrique <pro.henriquerossin@gmail.com>
2025-11-24 11:57:42 -0300

    add finalizacoes

--- Locadora.View/Program.cs
+++ Locadora.View/Program.cs
@@ -1,5 +1,12 @@
 ﻿using Locadora.View.Menus;
 
-MenuPrincipal menuPrincipal = new MenuPrincipal();
-try { menuPrincipal.Run(); }
-catch(Exception e) { Console.WriteLine(e); }
+MenuPrincipal menuPrincipal = new();
+
+try 
+{ 
+    menuPrincipal.Run(); 
+}
+catch (Exception e) 
+{ 
+    Console.WriteLine(e); 
+}
